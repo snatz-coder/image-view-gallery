c3425b01e68863deb19b1495a7c3929397eabc3d
sujane <snazlopez@gmail.com>
2021-05-23 06:04:40 +0530

    created gallery image slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { GalleryComponent } from './gallery/gallery.component';
+import { GalleryComponent } from './gallery/gallery.component' ;
+import { NgImageSliderModule } from 'ng-image-slider';
+
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    CommonModule,
+    RouterModule.forRoot([
+      { path: '', component: GalleryComponent }
+    ]),
+    BrowserAnimationsModule,
+    NgImageSliderModule
+  ],
   declarations: [
     AppComponent,
     GalleryComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: []
 })
 export class AppModule { }
